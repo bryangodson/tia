c5f5eeb436f4229efee45251667fd648cb46a621
bryangodson <ppdnquh@gmail.com>
2023-11-20 22:42:16 +0000

    update

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hero_image = document.querySelector(".hero img");
-    document.addEventListener('scroll', (e) => {
-        let y_axis = window.scrollY;
-        hero_image.style.transform = `translateY(${y_axis * 0.25}px)`
-        
-    })
+  document.addEventListener("scroll", (e) => {
+    let y_axis = window.scrollY;
+    hero_image.style.transform = `translateY(${y_axis * 0.25}px)`;
+  });
+  ScrollReveal({
+    reset: true,
+  }).reveal(".readmore > div > h1", {
+    delay: 300,
+  });
+  ScrollReveal({
+    reset: true,
+  }).reveal(".readmore > div > p", {
+    delay: 400,
+  });
+  ScrollReveal({
+    reset: true,
+  }).reveal(".readmore > div > a", {
+    delay: 500,
+  });
 });
