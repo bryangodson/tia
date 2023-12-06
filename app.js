21edad63796cea6f5a866ed9ab33d945af93aad6
bryangodson <ppdnquh@gmail.com>
2023-12-06 23:00:43 +0000

    on 6th Dec. 2024

--- app.js
+++ app.js
@@ -4,44 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let y_axis = window.scrollY;
     hero_image.style.transform = `translateY(${y_axis * 0.5}px)`;
   });
-  const elements = [
-    {
-      el: ".readmore > div > h1",
-      delay: 300,
-    },
-    {
-      el: ".readmore > div > p",
-      delay: 400,
-    },
-    {
-      el: ".readmore > div > a",
-      delay: 450,
-    },
-    {
-      el: ".overlay h1",
-      delay: 300,
-    },
-    {
-      el: ".overlay p",
-      delay: 400,
-    },
-
-    {
-      el: ".overlay a:nth-child(1)",
-      delay: 450,
-    },
-    {
-      el: ".overlay a:nth-child(2)",
-      delay: 500,
-    },
-  ];
-
-  for (const obj in elements) {
-    let currentElement = elements[obj];
+  document.querySelectorAll(".reveal").forEach((element) => {
     ScrollReveal({
       reset: true,
-    }).reveal(currentElement.el, {
-      delay: currentElement.delay,
+    }).reveal(element, {
+      delay: 500,
     });
-  }
+  });
 });
